b3180e3981c23a19d45159dfb3b47a8d2501a67c
Devmadia <agathaphan@icloud.com>
2020-06-20 22:50:24 -0500

    moved javascript folder into appropriate location, updated main.js with logic for calendar app

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/* GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist*/
+
+// render currentDay with Moment.js at top of index.html
+
+/* locate class id, 
+use jQuery to select it, 
+create variable to define it, 
+introduce function to create it on page*/
+
+/* 'container' element-class can be used to create dynamic elements
+inside with jQuery to generate the time blocks, event blocks, 
+and save blocks:
+
+workday = 9-5 
+
+var hour = moment.js script for generating X-AM
+
+function to produce eight hours of task events on the index page
+
+ability to edit tasks, tasks automatically change highlighted color
+depending on current time of day and task hour having passed
+
+ability to save tasks and persist even after refresh*/
