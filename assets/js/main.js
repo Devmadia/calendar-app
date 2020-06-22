badf26460a36d96fddcb1fad1d54dbd226d6c497
Devmadia <agathaphan@icloud.com>
2020-06-21 23:55:00 -0500

    introduced loadTasks and ability to save tasks for persistence

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,11 @@ THEN the saved events persist*/
 /* https://devhints.io/datetime */
 var updateClock = function() {
     var presentDate = moment().format('MMMM Do YYYY, h:mm:ss a'); // renders date
-    //var currentTime = moment().format('h:mm:ss a');  // renders current time
-    $('#currentDay').text(presentDate);
+    $('#currentDay').text(presentDate); // pushes time to jumbotron
 }
-setInterval(updateClock,1000);
+setInterval(updateClock,1000); // refreshes every second
     
 // setting up time variables for use - https://momentjs.com/docs/#/get-set/
-
 var ninAmHr = moment().set('hour', 9);
 var tenAmHr = moment().set('hour', 10);
 var eleAmHr = moment().set('hour', 11);
@@ -33,6 +31,30 @@ var thrPmHr = moment().set('hour', 15);
 var fouPmHr = moment().set('hour', 16);
 var fivPmHr = moment().set('hour', 17);
 
+// some way to determine if time has passed for coloring events per guidelines
+
+function loadTasks(){
+    // 9AM 
+    let nineAm = localStorage.getItem("9AM");
+    if(!nineAm){
+        $(".ninAM").val(nineAm);
+    }
+    $(".ninAM").text(nineAm);
+
+    // 10AM
+    let tenAM = localStorage.getItem("10AM");
+    if(!tenAM){
+        $(".tenAM").val(tenAM);
+    }
+
+    $(".tenAM").text(tenAM);
+}
+// on click save-tasks for scheduler
+$("#nineth").on("click", function(){
+    localStorage.setItem("9AM", $("#niner").val());   // bug exists where using class calls back as 'undefined', using id causes task item not to persist
+})
+
+loadTasks();
 /*var eventDisplay() {
     // 
     let 9AMHr = localStorage.getItem("");
